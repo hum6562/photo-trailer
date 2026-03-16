eda8ffcbe18f60d5c6b9068ff3a5c4a0355eb44e
smnarrace <92661376+smnarrace@users.noreply.github.com>
2026-03-16 18:22:18 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _PhotoMapScreenState extends State<PhotoMapScreen> {
   int _lastSyncedIndex = -1;
 
   Set<Marker> _markers = {};
-  fianl List<Marker> _rawOverviewMarkers = []; 
+  final List<Marker> _rawOverviewMarkers = []; 
   
   Set<Polyline> _polylines = {};
   List<Trip> _trips = [];
